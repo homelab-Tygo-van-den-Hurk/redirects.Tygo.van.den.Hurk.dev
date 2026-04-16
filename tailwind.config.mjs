ee80930b9e74ae309df1e6e53ea9b96ed006e76a
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-04-16 13:18:54 +0200

    chore: astro init and add 404 page
    Initialized astro and made a 404 page compatible with GitHub pages.

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#1e90ff',
+      },
+    },
+  },
+  plugins: [],
+};
